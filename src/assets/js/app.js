6950fe42cf878c3d4317fe9c164cc5d05397f90c
Jonian Guveli <jonian@hardpixel.eu>
2017-10-29 22:31:32 +0200

    include components in main files

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,9 +4,12 @@ import whatInput from 'what-input';
 window.$ = $;
 
 import Foundation from 'foundation-sites';
+import { OffCanvasMenu } from './components/offcanvasMenu';
+
+Foundation.plugin(OffCanvasMenu, 'OffCanvasMenu');
+
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
 
 $(document).foundation();
