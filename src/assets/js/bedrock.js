4bc8cfc092ed1f15ffb8107b923b6f234a85aee3
Jonian Guveli <jonian@hardpixel.eu>
2017-11-16 13:05:32 +0200

    rearrange javascripts

---
+++ src/assets/js/bedrock.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import './lib/foundation-explicit-pieces';
+import './lib/bedrock-explicit-pieces';
+
+$(document).foundation();
---
+++ src/assets/js/lib/bedrock-explicit-pieces.js
@@ -0,0 +1,7 @@
+import { OffCanvasMenu } from '../components/offcanvasMenu';
+import { TinyMceEditor } from '../components/tinyMceEditor';
+import { TableCheckbox } from '../components/tableCheckbox';
+
+Foundation.plugin(OffCanvasMenu, 'OffCanvasMenu');
+Foundation.plugin(TinyMceEditor, 'TinyMceEditor');
+Foundation.plugin(TableCheckbox, 'TableCheckbox');
