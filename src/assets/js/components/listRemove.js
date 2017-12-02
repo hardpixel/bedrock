1200bf1921becc9b5794febd95e5e4799126882c
Jonian Guveli <jonian@hardpixel.eu>
2017-12-02 13:46:06 +0200

    add list remove plugin

---
+++ src/assets/js/components/listRemove.js
@@ -0,0 +1,87 @@
+'use strict';
+
+import $ from 'jquery';
+import { Plugin } from 'foundation-sites/js/foundation.plugin';
+
+/**
+ * ListRemove module.
+ * @module listRemove
+ */
+
+class ListRemove extends Plugin {
+  /**
+   * Creates a new instance of an list-remove.
+   * @class
+   * @name ListRemove
+   * @fires ListRemove#init
+   * @param {Object} element - jQuery object to initialize.
+   * @param {Object} options - Overrides to the default plugin settings.
+   */
+  _setup(element, options) {
+    this.className = 'ListRemove'; // ie9 back compat
+    this.$element = element;
+    this.options = $.extend({}, ListRemove.defaults, this.$element.data(), options);
+    this.activeItems = [];
+
+    this._events();
+  }
+
+  /**
+   * Adds event handlers to the list-remove.
+   * @function
+   * @private
+   */
+  _events() {
+    this.$element.find('.remove-button').off('click').on({
+      'click': this._handleClick.bind(this)
+    });
+
+    this.$element.find('[data-list-item]').off('.zf.trigger').on({
+      'remove.zf.trigger': this.select.bind(this)
+    });
+  }
+
+  /**
+   * Updates active items on changes.
+   * @function
+   * @private
+   */
+  _updateActiveItems() {
+    this.activeItems = this.$element.find('[data-list-item]');
+    this.$element.trigger('changed.zf.remove.list', [this.activeItems]);
+  }
+
+  /**
+   * Handles click events on items.
+   * @function
+   * @private
+   */
+  _handleClick(event) {
+    var target = $(event.currentTarget).parents('[data-list-item]:first');
+    target.trigger('remove.zf.trigger');
+  }
+
+  /**
+   * Remove list item
+   * @param {Object} event - Event object passed from listener.
+   * @private
+   */
+  remove(event) {
+    $(event.currentTarget).remove();
+    this._updateActiveItems();
+  }
+
+  /**
+   * Destroys the list-remove plugin.
+   * @function
+   */
+  _destroy() {
+    this.$element.off('.zf.remove.list');
+    this.$element.find('.remove-button').off('click');
+    this.$element.find('[data-list-item]').off('.zf.trigger');
+  }
+}
+
+ListRemove.defaults = {};
+
+export {ListRemove};
--- src/assets/js/lib/bedrock-explicit-pieces.js
+++ src/assets/js/lib/bedrock-explicit-pieces.js
@@ -2,8 +2,10 @@ import { OffCanvasMenu } from '../components/offcanvasMenu';
 import { TinyMceEditor } from '../components/tinyMceEditor';
 import { TableCheckbox } from '../components/tableCheckbox';
 import { ListSelect } from '../components/listSelect';
+import { ListRemove } from '../components/listRemove';
 
 Foundation.plugin(OffCanvasMenu, 'OffCanvasMenu');
 Foundation.plugin(TinyMceEditor, 'TinyMceEditor');
 Foundation.plugin(TableCheckbox, 'TableCheckbox');
 Foundation.plugin(ListSelect, 'ListSelect');
+Foundation.plugin(ListRemove, 'ListRemove');
