a730004bac6981bda009be1d3f6373c1e4b4133d
Jonian Guveli <jonian@hardpixel.eu>
2017-12-15 14:10:17 +0200

    update file input plugin

--- src/assets/js/components/fileInput.js
+++ src/assets/js/components/fileInput.js
@@ -38,10 +38,7 @@ class FileInput extends Plugin {
     this.$empty = this.$element.find('.dz-message');
     this.$input = this.$element.find('input[type="file"]');
 
-    if (this.$preview.children().length) {
-      this.$empty.hide();
-    }
-
+    this._toggleEmpty();
     this._events();
   }
 
@@ -51,30 +48,130 @@ class FileInput extends Plugin {
    * @private
    */
   _events() {
-    var input = this.$input.get(0);
+    this.$input.off('change').on({
+      'change': this._change.bind(this)
+    });
 
-    this.$input.on('change', function(event) {
-      if (input.files && input.files[0]) {
-        var reader = new FileReader();
+    this.$element.off('click', '[data-dz-remove]').on({
+      'click': this._remove.bind(this)
+    }, '[data-dz-remove]');
 
-        reader.onload = function(e) {
-          var preview = this.$item.clone();
-          preview.find('[dz-thumbnail]').attr('src', e.target.result);
+    this.$element.off('dragover').on({
+      'dragover': this._activate.bind(this)
+    });
 
-          this.$empty.hide();
-          this.$preview.html(preview);
-        }.bind(this);
+    this.$element.off('dragleave dragend').on({
+      'dragleave': this._deactivate.bind(this),
+      'dragend': this._deactivate.bind(this)
+    });
+  }
 
-        reader.readAsDataURL(input.files[0]);
-      }
-    }.bind(this));
+  /**
+   * Hides the empty state element.
+   * @function
+   * @private
+   */
+  _hideEmpty() {
+    this.$empty.hide();
+    this.$empty.addClass('hide');
+  }
 
-    this.$element.on('click', '[dz-remove]', function(event) {
-      event.preventDefault();
+  /**
+   * Shows the empty state element.
+   * @function
+   * @private
+   */
+  _showEmpty() {
+    this.$empty.show();
+    this.$empty.removeClass('hide');
+  }
 
-      this.$empty.show();
-      this.$preview.html('');
-    }.bind(this));
+  /**
+   * Toggles the empty state element.
+   * @function
+   * @private
+   */
+  _toggleEmpty() {
+    var children = this.$preview.children().length;
+    this._hideEmpty();
+
+    if (!children) {
+      this._showEmpty();
+    }
+  }
+
+  /**
+   * Creates preview for loaded file.
+   * @param {Object} event - Event object passed from listener.
+   * @function
+   * @private
+   */
+  _updatePreview(event) {
+    var preview = this.$item.clone();
+    preview.find('[data-dz-thumbnail]').attr('src', event.target.result);
+
+    this._deactivate();
+    this._hideEmpty();
+    this.$preview.html(preview);
+  }
+
+  /**
+   * Highlights the empty wrapper.
+   * @param {Object} event - Event object passed from listener.
+   * @private
+   * @function
+   */
+  _activate(event) {
+    if (event) {
+      event.stopPropagation();
+    }
+
+    this.$element.addClass('dz-drag-hover');
+  }
+
+  /**
+   * Un highlights the empty wrapper.
+   * @param {Object} event - Event object passed from listener.
+   * @private
+   * @function
+   */
+  _deactivate(event) {
+    if (event) {
+      event.stopPropagation();
+    }
+
+    this.$element.removeClass('dz-drag-hover');
+  }
+
+  /**
+   * Creates preview when input changes.
+   * @param {Object} event - Event object passed from listener.
+   * @function
+   * @private
+   */
+  _change(event) {
+    var input = event.target;
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = this._updatePreview.bind(this);
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  /**
+   * Removes preview and shows empty state.
+   * @param {Object} event - Event object passed from listener.
+   * @function
+   * @private
+   */
+  _remove(event) {
+    event.preventDefault();
+
+    this._deactivate();
+    this._showEmpty();
+    this.$preview.html('');
   }
 
   /**
@@ -83,7 +180,9 @@ class FileInput extends Plugin {
    * @private
    */
   _destroy() {
-
+    this.$input.off('change');
+    this.$element.off('click', '[data-dz-remove]');
+    this.$element.off(['dragover', 'dragleave', 'dragend']);
   }
 }
 
