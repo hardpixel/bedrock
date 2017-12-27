9407208aa0132148832a538f995d897082a366df
Jonian Guveli <jonian@hardpixel.eu>
2017-12-27 23:21:57 +0200

    add js-cookie

--- src/assets/js/bedrock.js
+++ src/assets/js/bedrock.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import whatInput from 'what-input';
 
 window.$ = $;
+window.Cookies = require('js-cookie');
 
 import './lib/foundation-explicit-pieces';
 import './lib/bedrock-explicit-pieces';
