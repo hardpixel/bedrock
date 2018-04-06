86f9268283b958cbfaa452057eddf9a90cadc117
Jonian Guveli <jonian@hardpixel.eu>
2018-04-06 19:42:31 +0300

    add support to place map plugin for markers from json

--- src/assets/js/components/placeMap.js
+++ src/assets/js/components/placeMap.js
@@ -32,6 +32,11 @@ class PlaceMap extends Plugin {
    */
   _init() {
     if (google !== 'undefined') {
+      this.$map = $('<div class="place-map-container"></div>');
+      this.markersUrl = this.options.markersUrl;
+
+      this.$element.append(this.$map);
+
       if (this.options.height) {
         this.$element.height(this.options.height);
       } else {
@@ -39,6 +44,10 @@ class PlaceMap extends Plugin {
       }
 
       this.map = new google.maps.Map(this.$element[0], this.options.map);
+
+      if (this.markersUrl) {
+        this._getMarkers();
+      }
     } else {
       console.log('Google Maps is not available! Please add Google Maps API.');
     }
@@ -54,13 +63,71 @@ class PlaceMap extends Plugin {
   }
 
   /**
+   * Gets value from object by dot notation.
+   * @param {Object} obj - Object to get the key value from.
+   * @param {String} path - Dot notated path to the key.
+   * @function
+   * @private
+   */
+  _getObjectValue(obj, path) {
+    return new Function('_', `return _.${path}`)(obj);
+  }
+
+  /**
+   * Gets markers JSON from url.
+   * @function
+   * @private
+   */
+  _getMarkers() {
+    $.ajax(this.markersUrl).done(function(response) {
+      if (this.options.markersKey) {
+        response = this._getObjectValue(response, this.options.markersKey);
+      }
+
+      this._addMarkers(response);
+    }.bind(this));
+  }
+
+  /**
+   * Adds markers to the map.
+   * @param {Array} data - Collection of marker data objects add markers from.
+   * @function
+   * @private
+   */
+  _addMarkers(data) {
+    $.each(data, function(index, data) {
+      var item = {
+        position: {
+          lat: this._getObjectValue(data, this.options.latKey),
+          lng: this._getObjectValue(data, this.options.lngKey)
+        }
+      };
+
+      this.addMarker(item);
+    }.bind(this));
+  }
+
+  /**
+   * Adds single marker to the map.
+   * @param {Object} data - Marker data object to build item from.
+   * @function
+   */
+  addMarker(data) {
+    if (data.position && data.position.lat && data.position.lng) {
+      var marker = new google.maps.Marker(data);
+      marker.setMap(this.map);
+    }
+  }
+
+  /**
    * Destroys the place-map plugin.
    * @function
    * @private
    */
   _destroy() {
-    this.$element.height('');
-    this.$element.removeClass(`responsive-embed ${this.options.ratio}`);
+    if (this.$map) {
+      this.$map.remove();
+    }
   }
 }
 
@@ -68,8 +135,8 @@ PlaceMap.defaults = {
   ratio: 'panorama',
   map: {
     center: {
-      lat: -33.8688,
-      lng: 151.2195
+      lat: 37.96418,
+      lng: 23.72259
     },
     zoom: 13
   }
