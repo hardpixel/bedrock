fa4d8e1ece2191d6d33886d8319a4669751d14f6
Jonian Guveli <jonian@hardpixel.eu>
2018-07-10 17:34:13 +0300

    rrule generator plugin updates

--- src/assets/js/components/rruleGenerator.js
+++ src/assets/js/components/rruleGenerator.js
@@ -23,11 +23,6 @@ class RruleGenerator extends Plugin {
     this.className = 'RruleGenerator'; // ie9 back compat
     this.$element = element;
     this.options = $.extend({}, RruleGenerator.defaults, this.$element.data(), options);
-    this.dateStart = null;
-    this.dateEnd = null;
-    this.timeStart = null;
-    this.timeEnd = null;
-    this.repeat = null;
 
     this._init();
   }
@@ -39,13 +34,14 @@ class RruleGenerator extends Plugin {
    */
   _init() {
     this.$input = this.$element.find('[data-rrule]');
-    this.$repeat = this.$element.find('[data-rrule="repeat"]');
+    this.$repeat = this.$element.find('[data-rrule="freq"]');
     this.$controls = this.$element.find('[data-rrule-controls]');
     this.$text = this.$element.find('[data-rrule-text]');
 
     this.$controls.hide();
 
     this._toggleControls();
+    this._updateRules();
     this._events();
   }
 
@@ -56,7 +52,7 @@ class RruleGenerator extends Plugin {
    */
   _events() {
     this.$input.off('change.zf.rrule.update').on({
-      'change.zf.rrule.update': this._setRules.bind(this)
+      'change.zf.rrule.update': this._updateRules.bind(this)
     });
 
     this.$repeat.off('change.zf.rrule.repeat').on({
@@ -65,20 +61,65 @@ class RruleGenerator extends Plugin {
   }
 
   /**
-   * Shows the revelant controls on repeat change.
+   * Converts date string to date object.
+   * @param {String} string - Date string.
+   * @private
+   * @function
+   */
+  _parseDate(string) {
+    if (string && string !== '') {
+      return new Date(string);
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Parses options from inputs to RRule options.
+   * @param {Object} options - Raw options object.
+   * @private
+   * @function
+   */
+  _parseRules(options) {
+    var freq = options['freq'].toUpperCase();
+    options['freq'] = RRule[freq];
+
+    options['dtstart'] = this._parseDate(options['dtstart']);
+    options['until'] = this._parseDate(options['until']);
+    options['count'] = parseInt(options['count']);
+
+    return options;
+  }
+
+  /**
+   * Creates an RRule object from input values.
+   * @param {Object} options - Options object.
+   * @private
+   * @function
+   */
+  _createRule(options) {
+    var rule = new RRule(options);
+    this.$text.text(rule.toText());
+  }
+
+  /**
+   * Updates the generated rule from inputs
    * @param {Object} event - Event object passed from listener.
    * @private
    * @function
    */
-  _setRules(event) {
-    this.$input.filter(':visible').each(function(index, el) {
+  _updateRules(event) {
+    var options = {};
+
+    this.$input.each(function(index, el) {
       var attr = $(el).attr('data-rrule');
       var value = $(el).val();
 
-      this[attr] = value;
+      options[attr] = value;
     });
 
-    console.log(this);
+    options = this._parseRules(options);
+    this._createRule(options);
   }
 
   /**
